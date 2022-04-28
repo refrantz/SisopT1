6cc8dd40067b928fd7625b625afda196ee6f6dbc
refrantz <renan262001@gmail.com>
2022-04-28 12:19:56 -0300

    memoria adicionada

--- Maquina.java
+++ Maquina.java
@@ -1,7 +1,5 @@
-package SisopT1;
-
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,16 +11,22 @@ public class Maquina {
     static List<String> codigo = new ArrayList<String>();
 
     public static void main (String args[]){
-        for (String linha:codigo){
 
-            Map<String, Integer> dados = new HashMap<String, Integer>();
+        //for (String linha:codigo){
+        String linha = args[0];
+
+            Map<String, Integer> dados = new LinkedHashMap<String, Integer>();
+            dados.put("cu", 1);
+            dados.put("cuc", 2);
+            dados.put("cucu", 3);
             String[] input = linha.split(" ");
             String op = input[0];
             String param = input[1];
 
             if(param.contains("#")){
-               
+                param = "" + dados.values().toArray()[Integer.parseInt(param.substring(1))];
             }
+            
             if (op.equals("ADD")){
                 acc += Integer.parseInt(param);
             }else if(op.equals("SUB")){
@@ -35,7 +39,7 @@ public class Maquina {
             }else if(op.equals("LOAD")){
                 acc = Integer.parseInt(param);
             }else if(op.equals("STORE")){
-                dados.put(param, acc);
+                dados.replace(param, acc);
 
             }else if(op.equals("BRANY")){
                 
@@ -49,6 +53,7 @@ public class Maquina {
             }else if(op.equals("SYSCALL")){
 
             }
+            System.out.println(acc);
         }
     }
-}
+//}
