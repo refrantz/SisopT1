3a7cb28e1ecb8e9d36caea941556a0787f685ab9
refrantz <renan262001@gmail.com>
2022-04-28 12:52:41 -0300

    labels e pulos adicionados

--- Maquina.java
+++ Maquina.java
@@ -1,27 +1,35 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Maquina {
-    
-    static int acc = 0;
-    static int pc = 0;
-
-    static List<String> codigo = new ArrayList<String>();
 
     public static void main (String args[]){
+
+        int acc = 0;
+        List<String> codigo = new ArrayList<String>();
+        Map<String, Integer> dados = new LinkedHashMap<String, Integer>();
+        Map<String, Integer> labels = new HashMap<String, Integer>();
+
+        dados.put("cu", 1);
+        dados.put("cuc", 2);
+        dados.put("cucu", 3);
         codigo.add(args[0]);
 
-        for (String linha:codigo){
+        for (int pc = 0; pc < codigo.size();pc++){
 
-            Map<String, Integer> dados = new LinkedHashMap<String, Integer>();
-            dados.put("cu", 1);
-            dados.put("cuc", 2);
-            dados.put("cucu", 3);
-            String[] input = linha.split(" ");
+            String[] input = codigo.get(pc).split(" ");
             String op = input[0];
             String param = input[1];
+
+            if(op.contains(":")){
+                op = input[1];
+                param = input[2];
+                labels.put(input[0], pc);
+            }
 
             if(param.contains("#")){
                 param = "" + dados.values().toArray()[Integer.parseInt(param.substring(1))];
@@ -42,16 +50,22 @@ public class Maquina {
                 dados.replace(param, acc);
 
             }else if(op.equals("BRANY")){
-                
-            }else if(op.equals("BRPOS")){
-
-            }else if(op.equals("BRZERO")){
-
-            }else if(op.equals("BRNEG")){
-
+                pc = labels.get(param);
+            }else if(op.equals("BRPOS") && acc > 0){
+                pc = labels.get(param);
+            }else if(op.equals("BRZERO") && acc == 0){
+                pc = labels.get(param);
+            }else if(op.equals("BRNEG") && acc < 0){
+                pc = labels.get(param);
 
             }else if(op.equals("SYSCALL")){
+                int paramC = Integer.parseInt(param);
 
+                if(paramC == 0){
+                    System.exit(0);
+                }else{
+                    int intervalo = ThreadLocalRandom.current().nextInt(0, 21);
+                }
             }
             System.out.println(acc);
         }
