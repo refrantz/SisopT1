fe8df2a626e3872063d764a837fed1d14cd90c4e
refrantz <renan262001@gmail.com>
2022-05-02 09:26:16 -0300

    leitura de uma pasta ao inves de uma arquivo e execucao de todos processos (sem escalonamento), o if else das funcoes aritmeticas ja esta sendo feito, verificando se o parametro tem #

--- Maquina.java
+++ Maquina.java
@@ -16,119 +16,119 @@ public class Maquina {
 
         int acc = 0;
         List<Processo> processos = new ArrayList<Processo>();
-        List<String> regiaoDados = new ArrayList<String>();
-        //List<String> codigo = new ArrayList<String>(); talvez seja melhor tratar as linhas de codigo individualmente por processo
-        Map<String, Integer> dados = new LinkedHashMap<String, Integer>();
-        Map<String, Integer> labels = new HashMap<String, Integer>();
 
         // ainda precisa mudar a estrutura para trabalhar com multiplos programas, faz parte da interface ( precisamos decidir como fazer isso ).
         try {
 
-            BufferedReader br = new BufferedReader(new FileReader("programas/prog1.txt"));
+            File pasta = new File("programas");
 
-            //Criando o processo e adicionando ele na lista de processos - importante ter uma lista de processos pro escalonador conseguir decidir qual rodar depois
-            Processo processo1 = new Processo(0);
-            processos.add(processo1);
+            for(File txt : pasta.listFiles()){
 
-            String linha = br.readLine().strip().toUpperCase();
+                BufferedReader br = new BufferedReader(new FileReader(txt));
 
-            while(linha != null){
+                //Criando o processo e adicionando ele na lista de processos - importante ter uma lista de processos pro escalonador conseguir decidir qual rodar depois
+                Processo processo = new Processo(0);
+                processos.add(processo);
 
-                if(linha.equals(".CODE")){
-                    linha = br.readLine().strip().toUpperCase();
-                    while(!linha.equals(".ENDCODE")){
-                        processo1.codigo.add(linha);
+                String linha = br.readLine().strip().toUpperCase();
+
+                while(linha != null){
+
+                    if(linha.equals(".CODE")){
                         linha = br.readLine().strip().toUpperCase();
+                        while(!linha.equals(".ENDCODE")){
+                            processo.codigo.add(linha);
+                            linha = br.readLine().strip().toUpperCase();
+                        }
                     }
-                }
 
-                if(linha.equals(".DATA")){
-                    linha = br.readLine().strip().toUpperCase();
-                    while(!linha.equals(".ENDDATA")){
-                        regiaoDados.add(linha);
+                    if(linha.equals(".DATA")){
                         linha = br.readLine().strip().toUpperCase();
+                        System.out.println(linha);
+                        while(!linha.equals(".ENDDATA")){
+                            String[] input = linha.split(" ");
+                            processo.dados.put(input[0], Integer.parseInt(input[1]));
+                            linha = br.readLine().strip().toUpperCase();
+                        }
                     }
+
+                    linha = br.readLine();
+                    if(linha != null){
+                        linha = linha.strip().toUpperCase();
+                    }
+
                 }
 
-                linha = br.readLine();
-                if(linha != null){
-                    linha = linha.strip().toUpperCase();
-                }
-
+                br.close();
             }
 
-            br.close();
+        }catch (Exception e) {
 
-        } catch (Exception e) {
-
-            e.printStackTrace();
+                e.printStackTrace();
 
         }
+        
+        
 
-        for (String linha : regiaoDados){
-            String[] input = linha.split(" ");
-            dados.put(input[0], Integer.parseInt(input[1]));
-        }
 
-        //mudar para ser feito pra todos os processos, se um programa perde processador e volta depois ele deve voltar na mesma linha de antes, entao o PC pode ficar guardado na classe Processo.
-        for (int pc = 0; pc < processos.get(0).codigo.size(); pc++){
 
-            String[] input = processos.get(0).codigo.get(pc).split(" ");
-            String op = input[0];
-            String param = input[1];
+        for(Processo processo : processos){
+            for (; processo.pc < processo.codigo.size(); processo.pc++){
 
-            if(op.contains(":")){
-                labels.put(op.substring(0, op.length()-1), pc-1);
-                op = input[1];
-                param = input[2];
-            }
-
-            if(param.contains("#")){
-                param = "" + Integer.parseInt(param.substring(1));
-            }
-            
-            if (op.equals("ADD")){
-                if(dados.containsKey(param)){ //esse IF ELSE tem que estar para todas as operações aritmeticas - talvez exista um jeito mais elegante de fazer isso
-                    acc += dados.get(param);
-                }else{
+                String[] input = processo.codigo.get(processo.pc).split(" ");
+                String op = input[0].toUpperCase();
+                String param = input[1].toUpperCase();
+    
+                if(op.contains(":")){
+                    processo.labels.put(op.substring(0, op.length()-1), processo.pc-1);
+                    op = param;
+                    param = input[2].toUpperCase();
+                }
+    
+                if(param.contains("#")){
+                    param = "" + Integer.parseInt(param.substring(1));
+                }
+                
+                if (op.equals("ADD")){
                     acc += Integer.parseInt(param);
+                }else if(op.equals("SUB")){
+                    acc -= Integer.parseInt(param);
+                }else if(op.equals("MULT")){
+                    acc *= Integer.parseInt(param);
+                }else if(op.equals("DIV")){
+                    acc /= Integer.parseInt(param);
+    
+                }else if(op.equals("LOAD")){
+                    if(processo.dados.containsKey(param)){
+                        acc = processo.dados.get(param);
+                    }else{
+                        acc = Integer.parseInt(param);
+                    }
+                }else if(op.equals("STORE")){
+                    processo.dados.replace(param, acc);
+    
+                }else if(op.equals("BRANY")){
+                    processo.pc = processo.labels.get(param);
+                }else if(op.equals("BRPOS") && acc > 0){
+                    processo.pc = processo.labels.get(param);
+                }else if(op.equals("BRZERO") && acc == 0){
+                    processo.pc = processo.labels.get(param);
+                }else if(op.equals("BRNEG") && acc < 0){
+                    processo.pc = processo.labels.get(param);
+    
+                }else if(op.equals("SYSCALL")){
+                    int paramC = Integer.parseInt(param);
+    
+                    if(paramC == 0){
+                        System.exit(0);
+                    }else if(paramC == 1){
+                        //System.out.println(acc);
+                        int intervalo = ThreadLocalRandom.current().nextInt(0, 21);
+                    }
                 }
-            }else if(op.equals("SUB")){
-                acc -= Integer.parseInt(param);
-            }else if(op.equals("MULT")){
-                acc *= Integer.parseInt(param);
-            }else if(op.equals("DIV")){
-                acc /= Integer.parseInt(param);
-
-            }else if(op.equals("LOAD")){
-                if(dados.containsKey(param)){
-                    acc = dados.get(param);
-                }else{
-                    acc = Integer.parseInt(param);
-                }
-            }else if(op.equals("STORE")){
-                dados.replace(param, acc);
-
-            }else if(op.equals("BRANY")){
-                pc = labels.get(param);
-            }else if(op.equals("BRPOS") && acc > 0){
-                pc = labels.get(param);
-            }else if(op.equals("BRZERO") && acc == 0){
-                pc = labels.get(param);
-            }else if(op.equals("BRNEG") && acc < 0){
-                pc = labels.get(param);
-
-            }else if(op.equals("SYSCALL")){
-                int paramC = Integer.parseInt(param);
-
-                if(paramC == 0){
-                    System.exit(0);
-                }else if(paramC == 1){
-                    //System.out.println(acc);
-                    int intervalo = ThreadLocalRandom.current().nextInt(0, 21);
-                }
+                System.out.println(acc);
             }
-            System.out.println(acc);
         }
+        
     }
 }
--- Processo.java
+++ Processo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,7 +8,8 @@ public class Processo {
     public String pid; //aka nome, poder ser só um numero identificador como podemos colocar Processo #N, checar isso depois
     public static int nupid;
     public List<String> codigo = new ArrayList<String>();
-    public Map<String, Integer> dados = new LinkedHashMap<String, Integer>(); //talvez nao precise, talvez é melhor usar uma area de dados global, apenas ficar atento para variaveis com mesmo nome
+    public Map<String, Integer> dados = new LinkedHashMap<String, Integer>();
+    public Map<String, Integer> labels = new HashMap<String, Integer>();
     public Estado estado = Estado.PRONTO; // Inicializa como PRONTO
     public Prioridade prioridade = Prioridade.BAIXA; // BAIXA como padrão
     public Integer arrivalTime;
@@ -21,6 +23,7 @@ public class Processo {
         nupid++;
 
         this.arrivalTime = arrivalTime;
+        this.pc = 0;
     }
 
     public Processo(Integer arrivalTime, Prioridade prioridade){
@@ -29,6 +32,7 @@ public class Processo {
 
         this.arrivalTime = arrivalTime;
         this.prioridade = prioridade;
+        this.pc = 0;
     }
 
 
